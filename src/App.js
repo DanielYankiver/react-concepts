87d71efc8470451440814d1dd4393cf7d093f6a1
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-06-11 15:22:27 -0400

    first commit here

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/73a53668-b492-41a1-6180-358d39776000/public" className="App-logo" alt="logo" />
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
+        </a> */}
       </header>
+      <body className='main-page'>
+        Hi I am the main page 
+      </body>
     </div>
   );
 }
